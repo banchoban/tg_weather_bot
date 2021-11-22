036a9d3f36d34c96ec1edbbbc5d441daa56b2a9b
banchoban <aleksei.bannikov@gmail.com>
2021-11-22 18:55:26 +0300

    Method for sending weather data to user

--- tg_bot.py
+++ tg_bot.py
@@ -1,9 +1,11 @@
 import json
-
 import asyncio
-import aiohttp
+
 from decouple import config
 from datetime import datetime  # TODO timezones
+
+from helpers.requester import make_request
+from weather_processor import get_current_weather
 
 API_ACCESS_TOKEN = config('TG_TOKEN')
 API_REQUEST_TPL = 'https://api.telegram.org/bot{}/{}'  # URL format: (API token, method name)
@@ -12,21 +14,7 @@ TODAY_DATE = datetime.utcnow().date()
 subscribers_list = [337886033]
 
 
-async def make_request(url: str, payload=None):
-    """Default method for making HTTP requests"""
-
-    if not payload:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url=url, timeout=10) as response:
-
-                text = await response.text()
-
-                return text
-    else:
-        return  # TODO POST request
-
-
-async def make_weather_mailing():
+async def make_weather_mailing():  # TODO
     while True:
         if datetime.utcnow().date() > TODAY_DATE:  # if we have next day TODO make it better
             for user in subscribers_list:
@@ -45,12 +33,29 @@ async def get_updates():
         api_url = f'{API_REQUEST_TPL.format(API_ACCESS_TOKEN, "getUpdates")}?offset={updates_offset}'
 
         response = await make_request(url=api_url)
-
         update = json.loads(response)
 
-        # TODO process
+        if update['result']:
+            updates_offset = update['result'][-1]['update_id']
+            await process_update(update['result'][-1])
 
         await asyncio.sleep(2.0)
+
+
+async def process_update(json_data: dict):
+    if 'location' in json_data['message']:
+
+        weather_data = await get_current_weather(json_data['message']['location'])
+        await send_message(chat_id=json_data['message']['chat']['id'], text=json.dumps(weather_data))
+    else:
+        await send_message(chat_id=json_data['message']['chat']['id'], text='Sorry, i didn\'t find geo data in your message. Try to send it again.')
+
+
+async def send_message(chat_id: int, text: str, data=None):  # TODO do we need to send files in messages?
+    api_url = API_REQUEST_TPL.format(API_ACCESS_TOKEN, 'sendMessage')
+    data = {'chat_id': chat_id, 'text': text}
+
+    await make_request(url=api_url, payload=data)
 
 
 def run_bot():
