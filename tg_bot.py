6f9c129b01bc333ed65fe750d38aa5ba257b0719
banchoban <aleksei.bannikov@gmail.com>
2021-11-23 15:38:14 +0300

    Added queue for incoming updates from telegram

--- tg_bot.py
+++ tg_bot.py
@@ -13,6 +13,7 @@ IMAGE_URL_TPL = 'http://openweathermap.org/img/wn/{}@2x.png'
 TODAY_DATE = datetime.utcnow().date()
 
 subscribers_list = [337886033]
+updates_queue = list()
 # TODO user's db
 
 
@@ -39,25 +40,33 @@ async def get_updates():
 
         if update['result']:
             updates_offset = update['result'][-1]['update_id'] + 1
-            await process_update(update['result'][-1])
+            updates_queue.extend(update['result'])
 
         await asyncio.sleep(2.0)
 
 
-async def process_update(json_data: dict):
-    if 'location' in json_data['message']:
+async def process_updates():
+    while True:
+        if not updates_queue:
+            await asyncio.sleep(3.0)
+            continue
 
-        weather_data = await get_current_weather(json_data['message']['location'])
-        weather_info = f'Current weather data for <b>{weather_data["city"]}</b>\n' \
-                       f'<b>Weather:</b> {weather_data["weather"]} {weather_data["icon"]}\n' \
-                       f'<b>Temperature:</b> {weather_data["temp"]} \xb0C. <b>Feels like:</b> {weather_data["feels_like"]} \xb0C.\n' \
-                       f'<b>Pressure:</b> {weather_data["pressure"]} P. <b>Humidity:</b> {weather_data["humidity"]} %.\n' \
-                       f'<b>Visibility:</b> {weather_data["visibility"]} km. <b>Wind:</b> {weather_data["wind_speed"]} m/s.\n' \
-                       f'<b>Sunrise:</b> {datetime.fromtimestamp(weather_data["sunrise"]).time()}. <b>Sunset:</b> {datetime.fromtimestamp(weather_data["sunset"]).time()}.'
+        json_data = updates_queue[0]
+        updates_queue.remove(json_data)
 
-        await send_message(chat_id=json_data['message']['chat']['id'], text=weather_info)
-    else:
-        await send_message(chat_id=json_data['message']['chat']['id'], text='Sorry, i didn\'t find geo data in your message. Try to send it again.')
+        if 'location' in json_data['message']:
+
+            weather_data = await get_current_weather(json_data['message']['location'])
+            weather_info = f'Current weather data for <b>{weather_data["city"]}</b>\n' \
+                           f'<b>Weather:</b> {weather_data["weather"]} {weather_data["icon"]}\n' \
+                           f'<b>Temperature:</b> {weather_data["temp"]} \xb0C. <b>Feels like:</b> {weather_data["feels_like"]} \xb0C.\n' \
+                           f'<b>Pressure:</b> {weather_data["pressure"]} P. <b>Humidity:</b> {weather_data["humidity"]} %.\n' \
+                           f'<b>Visibility:</b> {weather_data["visibility"]} km. <b>Wind:</b> {weather_data["wind_speed"]} m/s.\n' \
+                           f'<b>Sunrise:</b> {datetime.fromtimestamp(weather_data["sunrise"]).time()}. <b>Sunset:</b> {datetime.fromtimestamp(weather_data["sunset"]).time()}.'
+
+            await send_message(chat_id=json_data['message']['chat']['id'], text=weather_info)
+        else:
+            await send_message(chat_id=json_data['message']['chat']['id'], text='Sorry, i didn\'t find geo data in your message. Try to send it again.')
 
 
 async def send_message(chat_id: int, text: str, data=None):  # TODO do we need to send files in messages?
@@ -69,7 +78,7 @@ async def send_message(chat_id: int, text: str, data=None):  # TODO do we need t
 
 def run_bot():
     ioloop = asyncio.get_event_loop()
-    tasks = [ioloop.create_task(get_updates())]
+    tasks = [ioloop.create_task(get_updates()), ioloop.create_task(process_updates())]
     wait_tasks = asyncio.wait(tasks)
     ioloop.run_until_complete(wait_tasks)
     ioloop.close()
